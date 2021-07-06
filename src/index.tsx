9aff08403ec8b8e666523d497b44fec6adb46f30
Mateus Morais <mateus.batista010@gmail.com>
2021-07-06 12:57:06 -0300

    fixed positioning

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Modal } from './components/Modal';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Modal />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
